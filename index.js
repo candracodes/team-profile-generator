9c1a1bac8d287f928dde002c077fa6dc89b9e068
Candra Fried <candracodes@gmail.com>
2021-10-12 22:29:59 -0500

    Testing out a few prompts to ensure, at the very least, that something is working on the command line.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const generateHTML = require('./src/generateHTML');
 
 // TODO: Invoke installed packages
-const fs = require('fs'); 
+const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Invoke the parent class
@@ -16,7 +16,28 @@ const Intern = require("./lib/intern");
 // TODO: Create an empty array to access later
 
 // TODO: Create prompts for adding a manager
+const addManager = () => {
+    return inquirer
+        .prompt([{
+                type: "input",
+                message: "What is the manager on this team?",
+                name: "name",
+            },
+            {
+                type: "input",
+                message: "What is the office number for this manager?",
+                name: "officeNumber",
+            }
+        ])
+        .then(response => {
+            console.log('response', response);
+        });
+
+} // end addManager
 
 // TODO: Create prompts for adding an engineer
 
-// TODO: Create prompts for adding a intern
+// TODO: Create prompts for adding an intern
+
+// TODO: Call functions to initialize
+addManager();

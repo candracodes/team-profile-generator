b9c5d4ed4c9cc429eb92f619b73eb379a5d634b8
Candra Fried <candracodes@gmail.com>
2021-10-12 22:09:47 -0500

    Updating index.js with Class references and altering the way I retrieve cards from generateHTML.js

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+//TODO: Invoke the logic to generate team profile HTML
+const generateHTML = require('./src/generateHTML');
+
+// TODO: Invoke installed packages
+const fs = require('fs'); 
+const inquirer = require('inquirer');
+
 // TODO: Invoke the parent class
 const Employee = require("./lib/employee");
 
-// TODO: Initialize the employee
-const employee = new Employee();
+// TODO: Invoke all child classes
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+
+// TODO: Create an empty array to access later
+
+// TODO: Create prompts for adding a manager
+
+// TODO: Create prompts for adding an engineer
+
+// TODO: Create prompts for adding a intern
